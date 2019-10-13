2113dcaf4375c7d61ac8fd10e448a96f61498dd9
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-10-12 21:14:25 -0400

    merged proto and comm, log format modification;

--- pftp/utils/logger.py
+++ pftp/utils/logger.py
@@ -6,7 +6,7 @@ def logger():
     if not _logger.hasHandlers():
         _logger.setLevel(logging.INFO)
         func = inspect.currentframe().f_back.f_code
-        formatter = logging.Formatter('%(levelname)s %(funcName)s %(asctime)-15s %(message)s', '%Y-%m-%d %H:%M:%S')
+        formatter = logging.Formatter('%(levelname)s %(asctime)-15s %(funcName)s %(message)s', '%Y-%m-%d %H:%M:%S')
         console_handler = logging.StreamHandler()
         console_handler.setFormatter(formatter)
         _logger.addHandler(console_handler)
