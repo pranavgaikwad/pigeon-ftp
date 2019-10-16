668e7025430c80abcc59453052a6246aa44aeee6
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-10-15 21:53:33 -0400

    refactoring proto stuff

---
+++ pftp/proto/header.py
@@ -0,0 +1,34 @@
+class PFTPHeader(object):
+    """ defines header structure """
+    LEN_SEQ = 32
+    LEN_STYPE = 16
+    LEN_CHECKSUM = 16
+
+    def __init__(self):
+        self.seq = b'0'*PFTPHeader.LEN_SEQ
+        self.stype = b'0'*PFTPHeader.LEN_STYPE
+        self.checksum = b'0'*PFTPHeader.LEN_CHECKSUM
+
+    def __str__(self):
+        return "{}{}{}".format(self.seq, self.stype, self.checksum)
+
+    def __eq__(self, other):
+        return True if self.seq == other.seq and self.stype == other.stype and self.checksum == other.checksum else False
+
+    def to_bytes(self):
+        return self.seq+self.stype+self.checksum
+
+    @staticmethod
+    def from_bytes(header):
+        h = PFTPHeader()
+        try:
+            h.seq = header[:PFTPHeader.LEN_SEQ]
+            h.stype = header[PFTPHeader.LEN_SEQ:PFTPHeader.LEN_SEQ+PFTPHeader.LEN_STYPE]
+            h.checksum = header[PFTPHeader.LEN_SEQ+PFTPHeader.LEN_STYPE:PFTPHeader.LEN_SEQ +
+                                PFTPHeader.LEN_STYPE+PFTPHeader.LEN_CHECKSUM]
+        except (KeyError, ValueError):
+            raise MalformedHeaderError()
+        return h
+
+class MalformedHeaderError(Exception):
+    pass
---
+++ pftp/proto/sequence.py
@@ -0,0 +1,24 @@
+from struct import unpack
+from pftp.proto.header import PFTPHeader as Header
+
+class SequenceNumberGenerator(object):
+    """ Helps generate sequence numbers reliably 
+
+    Additionally, provides conversion to / from bytes to / from integer
+    """
+
+    def __init__(self):
+        self.seq = -1
+
+    def get_next(self):
+        self.seq += 1
+        return self.seq, self.from_int(self.seq)
+
+    def get_prev(self):
+        return self.seq-1, self.from_int(self.seq-1)
+
+    def get_current(self):
+        return self.seq, self.from_int(self.seq)
+
+    def from_int(self, seq):
+        return ("{0:0%db}" % Header.LEN_SEQ).format(seq).encode()
