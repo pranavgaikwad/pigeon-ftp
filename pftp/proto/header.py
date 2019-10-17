75d4102cfae6801ee857616c024d59e193e90444
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-10-16 20:19:52 -0400

    malformed header check on length

--- pftp/proto/header.py
+++ pftp/proto/header.py
@@ -2,7 +2,7 @@ class PFTPHeader(object):
     """ defines header structure """
     LEN_SEQ = 32
     LEN_STYPE = 16
-    LEN_CHECKSUM = 16
+    LEN_CHECKSUM = 16 
 
     def __init__(self):
         self.seq = b'0'*PFTPHeader.LEN_SEQ
@@ -10,7 +10,7 @@ class PFTPHeader(object):
         self.checksum = b'0'*PFTPHeader.LEN_CHECKSUM
 
     def __str__(self):
-        return "{}{}{}".format(self.seq, self.stype, self.checksum)
+        return (self.seq+self.stype+self.checksum).decode('utf-8')
 
     def __eq__(self, other):
         return True if self.seq == other.seq and self.stype == other.stype and self.checksum == other.checksum else False
@@ -21,16 +21,23 @@ class PFTPHeader(object):
     def to_bytes(self):
         return self.seq+self.stype+self.checksum
 
+    @classmethod
+    def size(cls):
+        return PFTPHeader.LEN_SEQ + PFTPHeader.LEN_STYPE + PFTPHeader.LEN_CHECKSUM
+
     @staticmethod
     def from_bytes(header):
         h = PFTPHeader()
-        try:
+        try:   
+            header = header[:PFTPHeader.size()]
+            if len(header) != PFTPHeader.size():
+                raise MalformedHeaderError
             h.seq = header[:PFTPHeader.LEN_SEQ]
             h.stype = header[PFTPHeader.LEN_SEQ:PFTPHeader.LEN_SEQ+PFTPHeader.LEN_STYPE]
             h.checksum = header[PFTPHeader.LEN_SEQ+PFTPHeader.LEN_STYPE:PFTPHeader.LEN_SEQ +
                                 PFTPHeader.LEN_STYPE+PFTPHeader.LEN_CHECKSUM]
-        except (KeyError, ValueError):
-            raise MalformedHeaderError()
+        except:
+            raise MalformedHeaderError
         return h
 
 class MalformedHeaderError(Exception):
