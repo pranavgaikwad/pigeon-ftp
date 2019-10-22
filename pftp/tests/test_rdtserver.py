44fd1c307347966e7d800a8f94624271137eb607
Pranav Gaikwad <gaikwad.pranav@gmail.com>
2019-10-22 14:35:45 -0400

    added error probability to tests

--- pftp/tests/test_rdtserver.py
+++ pftp/tests/test_rdtserver.py
@@ -8,19 +8,19 @@ class PFTPServerTest(unittest.TestCase):
     
     def setUp(self):
         self.server_addr = ('127.0.0.1', 9989)
-        self.mss = 8192
+        self.mss = 4000
         self.logger = logger()
         self.pftpclient = PFTPClient([self.server_addr], atimeout=0.5, mss=self.mss)
         return super().setUp()
     
     def test_rdt_recv(self):
         msg = lambda size : b'1'*size
-        msg_size = 10000000
+        msg_size = 1000000
         t1 = Thread(name='RFTPClient', target=self.pftpclient.rdt_send, args=[msg(msg_size),60])
         t1.start()
-        pftpserver = PFTPServer(addr=self.server_addr, mss=self.mss)
+        pftpserver = PFTPServer(addr=self.server_addr, mss=self.mss, err_prob=0.2)
         rcvd = b''
-        for data in pftpserver.rdt_recv(timeout=5):
+        for data in pftpserver.rdt_recv(timeout=40):
             rcvd += data
         t1.join()
         self.assertEqual(rcvd, msg(msg_size))
