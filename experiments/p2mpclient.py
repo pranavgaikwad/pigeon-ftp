d93a39ad84901686c4962ab5880c30b6d1712000
Jaymin Desai <jddesai2@ncsu.edu>
2019-10-29 16:37:56 -0400

    experiments

--- experiments/p2mpclient.py
+++ experiments/p2mpclient.py
@@ -5,19 +5,17 @@ from pftp.client.pftpclient import PFTPClient
 
 if __name__ == "__main__":
     n = 3
-
-    print('Starting P2MPClient Task 1 for n:', n)
-
-    MSS = int(sys.argv[1])
+    MSS = 500
     server_port = 7735
     atimeout = 0.05
-    servers = [('152.46.19.48', 7741),
-               ('152.46.19.48', 7742),
-               ('152.46.19.48', 7743),
-               ('152.46.19.48', 7738),
-               ('152.46.19.48', 7739)]
 
-    with open('../data.txt', 'rb') as file:
+    servers = [('152.46.19.167', server_port),
+               ('152.46.19.168', server_port),
+               ('152.46.19.169', server_port),
+               ('192.168.10.62', server_port),
+               ('192.168.10.172', server_port)]
+
+    with open('data.txt', 'rb') as file:
         data = file.read()
 
     input('Press Enter to start P2MPClient with n: {}, MSS: {}, atimeout: {}'.format(n, MSS, atimeout))
@@ -26,4 +24,4 @@ if __name__ == "__main__":
     sent = PFTPClient(servers[:n], mss=MSS, atimeout=atimeout).rdt_send(data)
 
     print('SENT {} bytes'.format(sent))
-    print('TIME TAKEN', time() - start)
+    print('TIME TAKEN {:.2f} seconds'.format(time() - start))
